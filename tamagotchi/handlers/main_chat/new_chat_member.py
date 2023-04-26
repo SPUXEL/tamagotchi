0bd8b0794f348bb107b2c6e6f9dfa54b521a4225
Святослав Неверов <spuxel@yandex.ru>
2023-04-27 01:09:09 +0300

    Случайное приветствие вступивших в чат

--- tamagotchi/handlers/main_chat/new_chat_member.py
+++ tamagotchi/handlers/main_chat/new_chat_member.py
@@ -1,15 +1,21 @@
 
+import random
+
 from aiogram.types import ContentType, Message
 
 from _bot import bot, dispatcher
+from data import langpack
 
 
 @dispatcher.message_handler(
     content_types=[ContentType.NEW_CHAT_MEMBERS]
 )
 async def new_chat_member(update: Message):
-    new_member = update.new_chat_members[0]
+    member = update.new_chat_members[0]
+
+    greeting = random.choice(langpack.GREETINGS)
+
     await bot.send_message(
         update.chat.id,
-        f"Добро пожаловать {new_member.mention}"
+        greeting.replace("*", member.mention)
     )
