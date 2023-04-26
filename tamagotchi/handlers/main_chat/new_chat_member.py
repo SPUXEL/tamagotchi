1671c0c0c57b422975ea2dfbc04fcc2572001b9f
Святослав Неверов <spuxel@yandex.ru>
2023-04-27 01:37:33 +0300

    Рефакторинг файла handlers.main_chat.new_chat_member

--- tamagotchi/handlers/main_chat/new_chat_member.py
+++ tamagotchi/handlers/main_chat/new_chat_member.py
@@ -4,10 +4,11 @@ import random
 from aiogram.types import ContentType, Message
 
 from _bot import bot, dispatcher
-from data import langpack
+from data import config, langpack
 
 
 @dispatcher.message_handler(
+    chat_id=config.MAIN_CHAT_ID,
     content_types=[ContentType.NEW_CHAT_MEMBERS]
 )
 async def new_chat_member(update: Message):
@@ -16,6 +17,6 @@ async def new_chat_member(update: Message):
     greeting = random.choice(langpack.GREETINGS)
 
     await bot.send_message(
-        update.chat.id,
+        config.MAIN_CHAT_ID,
         greeting.replace("*", member.mention)
     )
